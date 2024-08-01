dabe16138d3dc856d3fb90bae1b242646811b003
NabinGhimire0 <nabinghimire002@gmail.com>
2024-08-01 08:33:40 +0545

    2ndsemworkshop

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#216869',
+        secondary:'#F4F803',
+      }
+    },
+  },
+  plugins: [],
+}
+
